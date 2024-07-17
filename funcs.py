915c026fda2783b6ed859bed130d4e52093541b7
teahouse15 <pasterziv@gmail.com>
2024-07-17 22:57:28 +0800

    update.

--- funcs.py
+++ funcs.py
@@ -6,6 +6,48 @@ import json
 
 from define import Hero, Gear
 
+main_team = {
+    '马超': [
+        ['马超', '张辽', '曹操'],
+        ['马超', '魏延', '曹操'],
+        ['公孙瓒', '马超', '张辽'],
+        ['曹纯', '马超', '张辽']
+    ],
+    '吕布': [
+        ['吕布', '貂蝉', '张机'],
+        ['吕布', '张机', '孙权'],
+        ['吕布', '张机', '魏延'],
+        ['吕布', '吕蒙', '田丰']
+    ],
+    '大乔': [
+
+    ],
+    '杜预': [
+
+    ],
+    '荀彧': [
+
+    ],
+    '马岱': [
+
+    ],
+    '关羽': [
+
+    ],
+    '华雄': [
+
+    ],
+
+}
+start_team = (
+
+)
+
+
+
+
+
+
 
 def get_account_ordersn(url):
     parsed_url = urlparse(url)
@@ -23,7 +65,7 @@ def parse_hero(json_data: Dict[str, Any]) -> Hero:
 
 
 def test():
-    url = 'https://stzb.cbg.163.com/cgi/mweb/equip/1/202406281002116-1-WSVKTHNQ5KWZ9D?view_loc=equip_list%7Ctag_key%3A%7B%22is_from_ad_reco%22%3A%200,%20%22tag%22%3A%20%22general_rec_din_tfs%22%7D&reco_request_id=1721200271025evUgP&tag=general_rec_din_tfs'
+    url = 'https://stzb.cbg.163.com/cgi/mweb/equip/1/202406291702116-1-WTWOLCJRCESBAO?view_loc=equip_list%7Ctag_key%3A%7B%22is_from_ad_reco%22%3A%200,%20%22tag%22%3A%20%22general_rec_din_tfs%22%7D&reco_request_id=17212101443301x1j6&tag=general_rec_din_tfs'
     ordersn = get_account_ordersn(url)
     data = {
         'ordersn': ordersn,
@@ -34,21 +76,42 @@ def test():
     account_hero_information = json.loads(account_total['equip_desc'])
 
     hero_list = account_hero_information['card']
-    print(get_hero_list(hero_list))
-    # for hero in hero_list:
-    #     # 使用 parse_hero 函数将 JSON 数据解析为 Hero 类的实例
-    #     hero = parse_hero(hero)
-    #
-    #     # 输出 Hero 实例的属性
-    #     print(hero.__dict__)
+    for hero_list in get_hero_list(hero_list):
+        print(hero_list)
 
 
 def get_hero_list(hero_list):
-    hero_name_list = []
+    """
+    :param hero_list: JSON信息
+    :return: tuple(普通武将, XP武将, SP武将)
+    """
+    nu_hero_list = []
+    xp_hero_list = []
+    sp_hero_list = []
+    s2_hero_list = []
+    s3_hero_list = []
     for hero in hero_list:
         if hero['quality'] == 5:
-            hero_name_list.append(hero['name'])
-    return hero_name_list
+            if hero['season'] == 'SP':
+                sp_hero_list.append(hero['name'])
+            elif hero['season'] == 'XP':
+                xp_hero_list.append(hero['name'])
+            elif hero['season'] == 'S2':
+                s2_hero_list.append(hero['name'])
+            elif hero['season'] == 'S3':
+                s3_hero_list.append(hero['name'])
+            else:
+                nu_hero_list.append(hero['name'])
+    return s2_hero_list, s3_hero_list, xp_hero_list, sp_hero_list, nu_hero_list
+
+
+def team_check(hero):
+
+
+    core_heao = ['马超', '']
+
+
+
 
 
 if __name__ == '__main__':
---
+++ pic.py
@@ -0,0 +1,29 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PySide6.QtGui import QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("区域展示图片示例")
+
+        # 创建 QGraphicsView 和 QGraphicsScene
+        self.view = QGraphicsView(self)
+        self.scene = QGraphicsScene(self)
+
+        # 加载图片并添加到场景
+        pixmap = QPixmap("res/cards/medium/hero/card_medium_100003.jpg")  # 替换为图片路径
+        scaled_pixmap = pixmap.scaled(133, 197)  # 设置为130x200大小
+        pixmap_item = QGraphicsPixmapItem(scaled_pixmap)
+        self.scene.addItem(pixmap_item)
+
+        # 设置场景和视图
+        self.view.setScene(self.scene)
+        self.setCentralWidget(self.view)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.resize(1100, 800)
+    window.show()
+    sys.exit(app.exec())
